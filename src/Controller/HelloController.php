2d737d2e263cbb87354cf0b02bf3bba1fb874f20
Kamo Yepremyan <yepremyan2018@gmail.com>
2021-11-22 15:41:01 +0100

    setup annotation bundle creat hello route

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,11 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class HelloController
 {
 
+
+  /**
+   * @Route("/", name="index")
+   */
   public function index()
   {
     dd("ça function");
+  }
+
+  /**
+   * @Route("/hello/{name?World}", name="hello")
+   */
+  public function hello($name): Response
+  {
+    // dd("Hello World");
+
+    return new Response("Hello $name");
   }
 }
